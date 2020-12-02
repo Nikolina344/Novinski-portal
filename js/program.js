a8e08dcad6f580c1ba512f30520364ef2a244e14
Nikolina344 <ninasapke@gmail.com>
2020-12-02 13:36:04 +0100

    My project

---
+++ js/program.js
@@ -0,0 +1,10 @@
+$(document).ready(function () { 
+    
+   
+    $("form").submit(function(){
+        alert("Uspešno ste poslali poruku! Odgovor ćete dobiti u najskorijem roku!");
+    }); 
+	
+	
+	
+});
